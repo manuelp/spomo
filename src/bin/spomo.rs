22fc63ee58095b5a830abab670fc9d3f67c847f3
Manuel Paccagnella <manuel.paccagnella@pm.me>
2025-10-13 10:42:27 +0200

    Single countdown

--- src/bin/spomo.rs
+++ src/bin/spomo.rs
@@ -1,19 +1,38 @@
-use spomo::error::{AppResult, AppError};
+use std::env;
+use std::time::{Duration, Instant};
+
+use error_stack::ResultExt;
+use spomo::error::{AppError, AppResult};
+use spomo::feature;
 use spomo::init;
-use error_stack::bail;
-use tracing::{error, info, trace, warn};
+
+fn read_duration() -> AppResult<Duration> {
+    let cli_arguments: Vec<_> = env::args().map(|a| a.to_owned()).collect();
+    let duration_spec = cli_arguments.get(1).ok_or(AppError)?;
+    feature::duration_parsing::parse_duration(duration_spec)
+        .change_context(AppError)
+        .attach("cannot parse duration spec")
+}
 
 fn main() -> AppResult<()> {
     init::error_reporting();
     init::tracing();
 
-    println!("Hello");
+    let duration_spec = read_duration()?;
 
-    info!("Sample log message");
-    warn!("Warning message");
-    error!("Error message");
-    trace!("Tracing message");
+    let started = Instant::now();
+    let end_time = started + duration_spec;
+    loop {
+        let now = Instant::now();
+        let elapsed_time = now.duration_since(started);
+        let remaining_time = end_time.duration_since(now);
+        println!("Remaining: {:?}, elapsed: {:?}", remaining_time, elapsed_time);
+        std::thread::sleep(Duration::from_secs(1));
+        if elapsed_time > duration_spec {
+            break;
+        }
+    }
+    println!("DING!");
 
-    //bail!(AppError);
     Ok(())
 }
--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,7 @@
 use error_stack::Report;
 
 #[derive(Debug, thiserror::Error)]
-#[error("An application error has occured")]
+#[error("an application error has occured")]
 pub struct AppError;
 
 pub type AppResult<T> = Result<T, Report<AppError>>;
